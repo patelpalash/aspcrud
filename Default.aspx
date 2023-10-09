eb25cdd31d3cdfa0526297084bc8e6c8287919fd
palash <patelpalash57@gmail.com>
2023-10-09 15:19:40 +0530

    crud operation in asp done

---
+++ Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 458px;
+        }
+    </style>
+</head>
+<body style="height: 486px">
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <asp:Label ID="id" runat="server" Text="id"></asp:Label>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtid" runat="server"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="name"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btninsert" runat="server" Text="btninsert" />
+            <asp:Button ID="btnupdate" runat="server" Text="update" />
+            <asp:Button ID="btndelete" runat="server" Text="delete" />
+            <asp:Button ID="btnsearch" runat="server" Text="search" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="mobile"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtmobile" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" EnableModelValidation="True" Width="571px">
+                <Columns>
+                    <asp:BoundField DataField="uid" HeaderText="uid" SortExpression="uid" />
+                    <asp:BoundField DataField="uname" HeaderText="uname" SortExpression="uname" />
+                    <asp:BoundField DataField="mobile" HeaderText="mobile" SortExpression="mobile" />
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" ProviderName="<%$ ConnectionStrings:ConnectionString.ProviderName %>" SelectCommand="SELECT [uid], [uname], [mobile] FROM [crud]"></asp:SqlDataSource>
+            <asp:AccessDataSource ID="AccessDataSource1" runat="server"></asp:AccessDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,93 @@
+﻿
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Web.UI.MobileControls
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Dim cn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=B:\3 year 5 sem\external exam\aspcrud\Database7.accdb")
+
+    Dim cmd As OleDbCommand
+    Dim dr As OleDbDataReader
+    Dim adp As New OleDbDataAdapter
+    Dim ds As New DataSet
+    Dim no As Integer
+    Dim drow As DataRow
+    Dim qry As String
+    Protected Sub btninsert_Click(sender As Object, e As EventArgs) Handles btninsert.Click
+        Try
+            Dim str As String
+            Dim ans As Integer
+            str = "insert into crud(uid,uname,mobile) values(" & txtid.Text & ",'" & txtname.Text & "'," & txtmobile.Text & " ) "
+            cn.Open()
+            Dim cmd As New OleDbCommand(str, cn)
+            ans = cmd.ExecuteNonQuery
+
+
+            MsgBox("Record Inserted Successfully : " & ans)
+            GridView1.DataBind()
+            cn.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+
+    Protected Sub btnupdate_Click(sender As Object, e As EventArgs) Handles btnupdate.Click
+        Try
+            Dim str As String
+            Dim ans As Integer
+            str = "Update crud set uname='" & txtname.Text & "',mobile=" & txtmobile.Text & " where uid=" & txtid.Text & "   "
+            Dim cmd As New OleDbCommand(str, cn)
+            cn.Open()
+            ans = cmd.ExecuteNonQuery
+            MsgBox("Record Updated Successfully : " & ans)
+            GridView1.DataBind()
+            cn.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Protected Sub btndelete_Click(sender As Object, e As EventArgs) Handles btndelete.Click
+        Try
+            Dim str As String
+            Dim ans As Integer
+            str = "Delete from crud where uid=" & txtid.Text & "  "
+            Dim cmd As New OleDbCommand(str, cn)
+            cn.Open()
+            ans = cmd.ExecuteNonQuery
+            cn.Close()
+            MsgBox("No of Records are : " & ans)
+            GridView1.DataBind()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnsearch_Click(sender As Object, e As EventArgs) Handles btnsearch.Click
+        Try
+            cn.Open()
+            Dim str As String
+            str = "Select * from crud where uid=" & txtid.Text & " "
+            Dim cmd As New OleDbCommand(str, cn)
+            Dim dr As OleDbDataReader
+            dr = cmd.ExecuteReader
+            While dr.Read
+                txtname.Text = dr.Item(1).ToString
+                txtmobile.Text = dr.Item(2).ToString
+
+
+            End While
+            GridView1.DataBind()
+            cn.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+End Class
